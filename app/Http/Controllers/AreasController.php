9c2ef4639a0710ddbef4450054bd9ee605fbb8e6
CradleTechnologies <kelvin.sankale@strathmore.edu>
2024-04-08 12:42:12 +0300

    Transport App project for Fullstack programming - Backend

---
+++ app/Http/Controllers/AreasController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Areas;
+use Illuminate\Http\Request;
+
+class AreasController extends Controller
+{
+    public function createArea(Request $request){
+        $request->validate([
+            "area_name"=>"required"
+        ]);
+
+        $area = Areas::create([
+            'area_name'=>$request->area_name,
+            'description'=>$request->description
+        ]);
+
+        return response()->json($area);
+    }
+}
